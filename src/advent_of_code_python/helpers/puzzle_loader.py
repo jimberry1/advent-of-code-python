5783bf547453d7822103ae9e3ddf75c832255cd5
jim.berry <jim.berry@signal-ai.com>
2025-12-04 13:48:40 +0000

    improvements to puzzle loader

--- src/advent_of_code_python/helpers/puzzle_loader.py
+++ src/advent_of_code_python/helpers/puzzle_loader.py
@@ -5,14 +5,8 @@ from typing import Any, Callable
 import requests
 
 
-def _ensure_path_exists(year):
-    input_path = Path(f"resources/aoc_inputs/{year}")
-    if not input_path.exists():
-        input_path.mkdir(parents=True, exist_ok=True)
-
-
-def _get_input_file_name(day_number: int, year: int = 2025):
-    return f"resources/aoc_inputs/{year}/input_{day_number}.txt"
+def _get_input_path(day_number: int, year: int = 2025):
+    return Path(f"resources/aoc_inputs/{year}/input_{day_number}.txt")
 
 
 def _fetch_input(day_number: int, year: int):
@@ -24,23 +18,29 @@ def _fetch_input(day_number: int, year: int):
         response = requests.get(url, headers=headers)
         response.raise_for_status()
 
-        _ensure_path_exists(year)
-
-        with open(_get_input_file_name(day_number, year), "w") as f:
+        with open(_get_input_path(day_number, year), "w") as f:
             f.write(response.text)
     except Exception as e:
         print(f"failed to fetch puzzle input for day {day_number} year {year}... {e}")
 
 
-def load_lines(day_number: int):
-    with open(_get_input_file_name(day_number), "r") as f:
+def get_input_file(day_number: int, year: int = 2025):
+    file_path = _get_input_path(day_number, year)
+    if not file_path.exists():
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        _fetch_input(day_number, year)
+    return file_path
+
+
+def load_lines(day_number: int, year: int = 2025):
+    with open(get_input_file(day_number), "r") as f:
         return [line.strip() for line in f]
 
 
 def load_lines_as_lists(
-    day_number: int, *, item_xf: Callable[[str], Any] | None = None
+    day_number: int, year: int = 2025, *, item_xf: Callable[[str], Any] | None = None
 ):
-    with open(_get_input_file_name(day_number), "r") as f:
+    with open(get_input_file(day_number), "r") as f:
         return [
             [char if not item_xf else item_xf(char) for char in line.strip()]
             for line in f
@@ -53,7 +53,7 @@ def load_line_as_list(
     split_on: str | None = None,
     item_xf: Callable[[str], Any] | None = None,
 ):
-    with open(_get_input_file_name(day_number), "r") as f:
+    with open(get_input_file(day_number), "r") as f:
         line = f.readline().strip()
         groups = line.split(split_on) if split_on else [line]
         return groups if not item_xf else [item_xf(group) for group in groups]
